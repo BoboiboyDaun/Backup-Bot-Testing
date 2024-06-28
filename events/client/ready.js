5d67de11559a57b1575d2f829cf3b6a9035abb5e
Zetoboy Maccalan 🇵🇸✊🏻 <111028372+BoboiboyDaun@users.noreply.github.com>
2024-06-28 10:56:47 +0700

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,6 +1,7 @@
 const statusActivity = require('./statusActivity');
+const { red, yellow, green, blue, cyan, purple } = require('colorotte');
 
 module.exports = async (client) => {
   statusActivity(client); // Set up bot status activity
-console.log(`Ready as ${client.user.tag} to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
+console.log(cyan(`Ready as ${client.user.tag} to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`));
 };
