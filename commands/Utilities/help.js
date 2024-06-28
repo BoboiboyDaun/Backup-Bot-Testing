1002e17e5e0ccc7e8e87c6040475c25929747274
Zetoboy Maccalan 🇵🇸✊🏻 <111028372+BoboiboyDaun@users.noreply.github.com>
2024-06-28 08:49:50 +0700

    Update commands

---
+++ commands/Utilities/help.js
@@ -0,0 +1,45 @@
+const { MessageEmbed } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  description: "Help Command!",
+  usage: "Help | <Command Name>",
+  run: async (client, message, args) => {
+    message.delete();
+    const readCommands = (dir) => {
+      const categories = fs.readdirSync(dir);
+
+      let embed = new MessageEmbed()
+        .setColor("BLUE")
+        .setTitle("⚙️ List Of Commands")
+        .setThumbnail(client.user.displayAvatarURL())
+        .setFooter(`Requested By ${message.author.username}`, message.author.displayAvatarURL())
+        .setTimestamp();
+
+      categories.forEach(category => {
+        const categoryPath = path.join(dir, category);
+        const commands = fs.readdirSync(categoryPath).filter(file => file.endsWith('.js'));
+
+        let commandString = "";
+        commands.forEach(file => {
+          const commandName = file.split('.')[0];
+          const command = require(path.join(categoryPath, file));
+          commandString += `\`${commandName}\`, `;
+        });
+
+        if (commandString.trim() !== "") {
+          embed.addField(category, commandString.slice(0, -2)); // Removes the last comma
+        }
+      });
+
+      return embed;
+    };
+      
+    const embed = readCommands(path.join(__dirname, '..'));
+      
+    if (!args.length) return message.channel.send(embed);
+  }
+};
