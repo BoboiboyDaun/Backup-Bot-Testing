61f1abce8775b710f45882c796c58b97af75fa30
Zetoboy Maccalan 🇵🇸✊🏻 <111028372+BoboiboyDaun@users.noreply.github.com>
2024-06-28 10:12:06 +0700

    Update

---
+++ inedx.js
@@ -0,0 +1,12 @@
+const { Client, Collection } = require('discord.js');
+const client = new Client();
+const config = require('./config.json');
+
+client.commands = new Collection();
+client.config = config;
+
+require('./handlers/loadEvent')(client);
+require('./handlers/loadCommand')(client);
+
+// Login
+client.login(config.TOKEN);
