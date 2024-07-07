b1e32bde25ba44e233da240c65a17b3e9e9ff679
Zetoboy Maccalan 🇵🇸✊🏻 <111028372+BoboiboyDaun@users.noreply.github.com>
2024-07-08 00:09:20 +0700

    Update events

---
+++ events/helper/disconnect.js
@@ -0,0 +1,16 @@
+const { red } = require('colorette');
+const { logMessage } = require('../../helpers/logHelper');
+const config = require('../../config.json');
+
+module.exports = (client, event) => {
+  const message = `Disconnected: ${event.code} - ${event.reason}`;
+  console.error(red(message));
+  logMessage(message);
+
+  // Reconnect logic
+  if (event.code !== 1000) {
+    setTimeout(() => {
+      client.login(config.TOKEN).catch(console.error);
+    }, 5000); // Retry after 5 seconds
+  }
+};
